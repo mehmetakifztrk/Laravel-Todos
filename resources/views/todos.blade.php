b5c7698ac34edf9e67539fd44884634a414695d6
mehmetakifztrk <mehmetakifztrk@outlook.com>
2019-02-08 08:01:04 +0300

    second commit

---
+++ resources/views/todos.blade.php
@@ -0,0 +1,24 @@
+@extends('layout')
+
+
+@section('content')
+
+    <div class="row">
+        <div class="col-lg-6 col-lg-offset-3">
+            <form action="/create/todo" method="post">
+                {{csrf_field()}}
+                <input type="text" class="form-control input-lg" name="todo" placeholder="Create a new todo">
+            </form>
+        </div>
+    </div>
+    <hr>
+    @foreach($todos as $todo)
+        {{$todo->todo}} <a href="{{route('todo.delete',['id' => $todo->id])}}" class="btn btn-danger"> delete </a>
+        {{csrf_field()}}
+        <a href="{{route('todo.update',['id' => $todo->id])}}" class="btn btn-info"> update </a>
+        @if(!$todo->completed)
+            <a href="/" class="btn btn-success"> Mark as completed </a>
+        @endif
+        <hr>
+    @endforeach
+@stop
